a922bff320724c7d8ea000375c29877a68672ed9
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-05-17 22:00:08 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2563eb', // blue-600
+          hover: '#1d4ed8', // blue-700
+        },
+      },
+    },
+  },
+  plugins: [],
+}
