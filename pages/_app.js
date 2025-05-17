dbb6e3d5135839f4fa899f308d668201c629e001
Itzfew <113174780+itzfew@users.noreply.github.com>
2025-05-17 23:53:53 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
-import Head from 'next/head';
 import '../styles/globals.css';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <script src="https://sdk.cashfree.com/js/v3/cashfree.test.js"></script>
-      </Head>
-      <ToastContainer position="top-center" autoClose={3000} />
-      <Component {...pageProps} />
-    </>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
